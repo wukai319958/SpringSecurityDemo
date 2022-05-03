42c4bc16b2dde1dc762792e7939b63577d9b5300
HHHeight <945546895@qq.com>
2022-05-03 15:24:35 +0800

    引入依赖

---
+++ src/main/java/com/wukai/SecurityApplication.java
@@ -0,0 +1,19 @@
+package com.wukai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SecurityApplication
+ *
+ * @author WuKai
+ * @date 2022/5/3
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
+
+
